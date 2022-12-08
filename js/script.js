0171fbcbf03b4ce2dcca61ae73e37aa0b35003aa
monibasz <monibashafiq123786@gmail.com>
2022-12-08 20:44:16 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let navbar = document.querySelector ('.header .navbar');
+
+document.querySelector('#menu-btn').onclick =() => {
+    navbar.classList.toogle('active');
+}
+
+window.onscroll =() =>{
+    navbar.classList.remove('active');
+}
